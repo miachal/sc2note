fa98f05bbece9b4b442fc9b11edc2e77cf945f83
miachal <61947247+miachal@users.noreply.github.com>
2020-04-23 23:51:22 +0200

    Working on ProfileBox

--- ui/src/Find.tsx
+++ ui/src/Find.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Select, Tag, Button } from 'antd';
+import { Select, Tag, Button, Progress } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
+import Level from './Level';
+import { example_data, example_data2 } from './mock';
 
 const StyledDiv = styled.div`
   width: 80%;
@@ -34,8 +36,51 @@ const tagRender = (props :any) => {
   );
 };
 
+interface IProfileBoxProps {
+  player: any;
+};
+
+const ProfileBoxContainer = styled.div`
+  width: 100%;
+  margin-top: 25px;
+  border: 3px solid #000;
+  display: flex;
+  background: #e0e0e0 
+`;
+
+const DataContainer = styled.div``;
+
+const NameContainer = styled.div`
+  font: 700 33px "Inconsolata";
+  color: #000;
+  margin-top: 5px;
+  margin-left: 10px;
+`;
+
+const LevelsBox = styled.div`
+  display: flex;
+  margin-top: 10px;
+`;
+
+const ProfileBox :React.FC<IProfileBoxProps> = (props) => (
+  <ProfileBoxContainer>
+    <img src={props.player.portrait} alt='profile' />
+    <div>
+    <DataContainer>
+      <NameContainer>[{props.player.clanTag}] {props.player.name}</NameContainer>
+    </DataContainer>
+    <LevelsBox>
+      {Object.keys(props.player.swarm).map(
+        race => <Level race={race} level={props.player.swarm[race]} />)
+      }
+    </LevelsBox>
+    </div>
+  </ProfileBoxContainer>
+);
+
 const Find :React.FC<IFindProps>  = () => {
   return (
+    <>
     <StyledDiv>
       <StyledSelect
       mode='tags'
@@ -49,6 +94,13 @@ const Find :React.FC<IFindProps>  = () => {
         Search
       </Button>
     </StyledDiv>
+      <div>
+        <ProfileBox player={example_data} />
+      </div>
+      <div>
+        <ProfileBox player={example_data2} />
+      </div>
+    </>
   );
 };
 
