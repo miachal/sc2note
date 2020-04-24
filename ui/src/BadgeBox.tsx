4d8b8fdb6cd72903824a4dd3194f563277693a9f
miachal <61947247+miachal@users.noreply.github.com>
2020-04-24 16:40:32 +0200

    add none to BadgeBox

--- ui/src/BadgeBox.tsx
+++ ui/src/BadgeBox.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 
-type LeagueType = 'bronze' | 'silver' | 'gold' | 'platinum' | 'diamond' | 'master' | 'grandmaster'
+type LeagueType = null | 'bronze' | 'silver' | 'gold' | 'platinum' | 'diamond' | 'master' | 'grandmaster'
 type TierType = 1 | 2 | 3 | 4
 type SizeType = 'small' | 'medium' | 'large'
 
 
 const Badge = styled.div<{ league: LeagueType }>`
-  background-image: ${props => `url(/img/badges/${props.league}.png)`};
+  background-image: ${({ league }) => league ? `url(/img/badges/${league}.png)` : 'url(/img/badges/none.png)'};
   background-repeat: no-repeat;
 `;
 
@@ -16,21 +16,21 @@ const Large = styled(Badge)<{ tier: TierType }>`
   width: 100px;
   height: 105px;
   flex: 0 0 100px;
-  background-position: 0 ${props => (props.tier-1) * (-105)}px;
+  background-position: 0 ${({ tier }) => (tier-1) * (-105)}px;
 `;
 
 const Medium = styled(Badge)<{ tier: TierType }>`
   width: 45px;
   height: 50px;
   flex: 0 0 45px;
-  background-position: -100px ${props => (props.tier-1) * (-50)}px;
+  background-position: -100px ${({ tier }) => (tier-1) * (-50)}px;
 `;
 
 const Small = styled(Badge)<{ tier: TierType }>`
   width: 25px;
   height: 30px;
   flex: 0 0 25px;
-  background-position: -145px ${props => (props.tier-1) * (-30)}px;
+  background-position: -145px ${({ tier }) => (tier-1) * (-30)}px;
 `;
 
 const CaptionText = styled.div`
