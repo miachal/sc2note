707e68b34afa9994e44190ce3f204b92241d5dd0
miachal <61947247+miachal@users.noreply.github.com>
2020-04-27 20:58:38 +0200

    add findComplexInformation to scraper

---
+++ api/src/scraper/scrape.js
@@ -0,0 +1,57 @@
+const {
+  searchPlayers,
+  searchTeams,
+  checkLadder,
+  findBnetId
+} = require('./rankedftw');
+const {
+  checkBattlenet
+} = require('./battlenet');
+
+
+async function findComplexInformation(players) {
+  const profiles = [];
+  for (let j = 0; j < players.length; ++j) {
+    const profile = await searchPlayers(players[j], true);
+    if (profile.length !== 1) {
+      return {
+        error: 'Couldnt find player with name ' + players[j]
+      };
+    }
+    profiles.push(profile[0].id);
+  }
+
+  const bnetProfiles = [];
+  for (let j = 0; j < profiles.length; ++j) {
+    const bnetId = await findBnetId(profiles[j]);
+    const bnetProfile = await checkBattlenet(bnetId);
+    bnetProfiles.push(bnetProfile);
+  }
+
+  const teams = await searchTeams(profiles[0]);
+
+  const commons = teams.filter(({
+    players
+  }) => {
+    const ids = players.map(p => p.id);
+    return JSON.stringify(ids) === JSON.stringify(profiles)
+  });
+
+  for (let j = 0; j < commons.length; ++j) {
+    const ladder = await checkLadder(commons[j]);
+    commons[j] = {
+      ...commons[j],
+      ...ladder
+    };
+    delete commons[j].players;
+  }
+
+  return {
+    players: bnetProfiles,
+    teams: commons
+  };
+}
+
+module.exports = {
+  findComplexInformation
+};
